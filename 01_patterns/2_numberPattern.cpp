7450518aca1c2e6554943ba356553a9428dd2af2
kishankumar110602 <153907603+kishankumar110602@users.noreply.github.com>
2025-11-30 19:03:03 +0000

    container config done

---
+++ 01_patterns/2_numberPattern.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+int main(){
+    int n = 5;
+
+
+    // code for the simple number pattern
+    // for(int i=1;i<n;i++){
+    //     for(int j=1;j<n;j++){
+    //         std::cout<<j;
+    //     }
+    //     std::cout<<std::endl;
+    // }
+
+    
+    //code using 
+    return 0;
+}
